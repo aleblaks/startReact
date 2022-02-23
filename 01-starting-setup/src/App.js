fec3d6b759066214cb31d070082462434432a5b8
alex <aleblaks@gmail.com>
2022-02-23 13:53:45 +0100

    third commit

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,6 +1,10 @@
-import Expenses from './components/Expenses';
+import React from 'react';
+import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
-function App() {
+
+
+const App = () => {
   const expenses = [
     {
       id: 'e1',
@@ -23,9 +27,23 @@ function App() {
     },
   ];
 
+  const addExpenseHandler = expense =>{
+    console.log('In App.js');
+    console.log(expense);
+  };
+
+  //return React.createElement(
+  //  'div',
+  //  {},
+  //React.createElement('h2', {}, "Let's get started!"),
+  //React.createElement(Expenses, { items: expenses })
+  //);
+  
+
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
